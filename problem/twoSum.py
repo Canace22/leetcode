65576ee8d65c8e1f9022e5b216afef05719b155e
Canace <canace22@qq.com>
2019-09-03 10:33:22 +0800

    add two sum solution

--- problem/twoSum.py
+++ problem/twoSum.py
@@ -1,9 +1,5 @@
-# leetcode-note
+# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
 
-> 每日算法练习记录
-
-1. 找出数组中相加等于目标值的两个元素下标
-```python
 # 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 
 # 示例:
@@ -25,17 +21,11 @@ class Solution:
             if complement in hashmap:
                 return [hashmap[complement], i]
             hashmap[val] = i
-    def _init_(self):
-       return self.twoSum([2, 7, 11, 15], 22)
 
+    def _init_(self):
+        print(self.twoSum([2, 7, 11, 15], 9))
+        return self.twoSum([2, 7, 11, 15], 9)
 
 
 x = Solution()
-print(x))
-```
-
-遍历列表，目标值与当前元素相减得到符合条件的元素，查找字典，如果存在符合条件的键，返回值，否则把该元素作为键，索引作为值存到字典里，重复以上步骤，直到找到对应的两个元素，返回索引列表
-
-
-
-
+x._init_()
