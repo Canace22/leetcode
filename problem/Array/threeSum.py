7666b23a811de9129294f0505b524be487ea468c
canace <canace22@qq.com>
2019-09-30 09:21:11 +0800

    update md

---
+++ problem/Array/threeSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeSum(self, nums):
+        # @nums: List[int]
+        # @return List[List[int]]
+        print(nums)
+
+    def __init__(self, nums):
+        print(nums)
+        # self.threeSum(nums)
+
+
+Solution([-1, 0, 1, 2, -1, -4])
