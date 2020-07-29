fb17187bd0c530d6f6a976ae72607d6211ac0dde
Canace <canace22@qq.com>
2020-07-29 10:27:02 +0800

    add  problem-base-type/findMedianSortedArrays.js

---
+++ problem-base-type/findMedianSortedArrays.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  // brute force
+  // 归并排序
+  const merged = [];
+  let i = 0;
+  let j = 0;
+  const [lenA, lenB] = [nums1.length, nums2.length];
+  while (i < lenA && j < lenB) {
+    if (nums1[i] < nums2[j]) {
+      merged.push(nums1[i]);
+      i++;
+    } else {
+      merged.push(nums2[j]);
+      j++;
+    }
+  }
+  while (i < lenA) {
+    merged.push(nums1[i]);
+    i++;
+  }
+  while (j < lenB) {
+    merged.push(nums2[j]);
+    j++;
+  }
+
+  const { length } = merged;
+  return length % 2 === 1
+    ? merged[Math.floor(length / 2)]
+    : (merged[length / 2] + merged[length / 2 - 1]) / 2;
+};
+
+var nums1 = [1, 3];
+var nums2 = [2];
+// var nums1 = [1, 2];
+// var nums2 = [3, 4];
+
+const res = findMedianSortedArrays(nums1, nums2)
+console.log(res);
+
