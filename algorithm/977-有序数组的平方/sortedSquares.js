5c9f27259024fbc994ae45aed46f4f23d49e5136
Canace <canace22@qq.com>
2020-10-16 18:04:19 +0800

    add:977-有序数组的平方

---
+++ algorithm/977-有序数组的平方/sortedSquares.js
@@ -0,0 +1,24 @@
+// 一刷时间: 2020-10-16
+// 来源: https://leetcode-cn.com/problems/squares-of-a-sorted-array/
+
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function (A) {
+  const len = A.length;
+  const result = [];
+  for (let i = 0, j = len - 1, pos = len - 1; i <= j; ) {
+    if (A[i] * A[i] > A[j] * A[j]) {
+      result[pos] = A[i] * A[i];
+      ++i;
+    } else {
+      result[pos] = A[j] * A[j];
+      --j;
+    }
+    --pos;
+  }
+
+  return result;
+};
+module.exports = sortedSquares;
---
+++ test/algorithm/977-有序数组的平方.test.js
@@ -0,0 +1,15 @@
+const sortedSquares = require('../../algorithm/977-有序数组的平方/sortedSquares');
+
+test('给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序', () => {
+  const input = [
+    [-4, -1, 0, 3, 10],
+    [-7, -3, 2, 3, 11]
+  ];
+  const output = [
+    [0, 1, 9, 16, 100],
+    [4, 9, 9, 49, 121]
+  ];
+  input.forEach((ele, i) => {
+    expect(sortedSquares(ele)).toEqual(output[i]);
+  });
+});
