4b9864b065a275558ca8874a090b1ad0ecbce93b
Canace <canace22@qq.com>
2021-06-09 13:36:10 +0800

    add:array/two-sum,array/find-repeat-num

---
+++ array/three-sum.js
@@ -0,0 +1,16 @@
+// https://leetcode-cn.com/problems/3sum/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    
+};
+
+const output1 = threeSum([-1, 0, 1, 2, -1, -4]);
+// output: [[-1,-1,2],[-1,0,1]]
+const output2 = threeSum([]);
+// output: []
+const output1 = threeSum([0]);
+// output: []
